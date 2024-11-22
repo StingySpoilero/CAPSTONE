e1b2da045f0b650f8466282644a9167415f4e675
StingySpoilero <dalaujon@gmail.com>
2024-11-22 09:51:20 -0600

    CAPSTONE

---
+++ CAPSTONE/src/pages/ReviewPage.jsx
@@ -0,0 +1,11 @@
+// src/pages/ReviewPage.jsx
+const ReviewPage = () => {
+    return (
+        <div>
+            <h2>User Reviews</h2>
+            <p>See what our users are saying about us!</p>
+        </div>
+    );
+};
+
+export default ReviewPage;
