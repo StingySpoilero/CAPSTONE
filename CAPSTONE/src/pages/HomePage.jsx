b1f91e7da5e21d2069b2ab0c55edd3fe8ce28d8d
StingySpoilero <dalaujon@gmail.com>
2024-11-22 09:25:29 -0600

    CAPSTONE

---
+++ CAPSTONE/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+// src/pages/HomePage.jsx
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Scheduling App</h1>
+            <p>Schedule your appointments easily and efficiently.</p>
+        </div>
+    );
+};
+
+export default HomePage;
