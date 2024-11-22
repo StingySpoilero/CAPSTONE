8ba8b992d1bf2c1f3f9c9a546a3bcd37302d8bfa
StingySpoilero <dalaujon@gmail.com>
2024-11-22 09:37:57 -0600

    CAPSTONE

---
+++ CAPSTONE/src/pages/ServicePage.jsx
@@ -0,0 +1,11 @@
+// src/pages/AboutPage.jsx
+const AboutPage = () => {
+    return (
+        <div>
+            <h2>About Us</h2>
+            <p>This application helps you manage your appointments seamlessly.</p>
+        </div>
+    );
+};
+
+export default AboutPage;
