7ec09726250c1e384621c8ebf72c8f76ddd0ca55
StingySpoilero <dalaujon@gmail.com>
2024-11-26 08:04:55 -0600

    CAPSTONE

--- CAPSTONE/src/HomePage.jsx
+++ CAPSTONE/src/HomePage.jsx
@@ -1,6 +1,6 @@
 // src/pages/HomePage.jsx
 import { getClients, createClient, deleteClient } from './api.mjs';
-import './HomePage.css'; // Import your CSS file
+import './HomePage.css'; 
 
 const HomePage = () => {
     return (
@@ -8,13 +8,18 @@ const HomePage = () => {
             <h1 className="centered-title">Wax the Ripper</h1>
             <p className="centered-text">Schedule your wax appointments easily and efficiently.</p>
             <img 
-                src="./images/Image3.jpeg" // Replace with your image path
+                src="./images/Image3.jpeg" 
                 alt="Description of Image 3"
                 className="homepage-image"
             />
             <img 
-                src="./images/Image8.jpg" // Replace with your image path
+                src="./images/Image8.jpg"
                 alt="Description of Image 8"
+                className="homepage-image"
+            />
+             <img 
+                src="./images/Image12.jpg"
+                alt="Description of Image 12"
                 className="homepage-image"
             />
         </div>
