98679b368730ef29e9f68bbd89dde40e45224920
StingySpoilero <dalaujon@gmail.com>
2024-11-26 07:52:12 -0600

    CAPSTONE

--- CAPSTONE/src/HomePage.jsx
+++ CAPSTONE/src/HomePage.jsx
@@ -1,11 +1,12 @@
 // src/pages/HomePage.jsx
 import { getClients, createClient, deleteClient } from './api.mjs';
+import './HomePage.css'; // Import the CSS file for styles
 
 const HomePage = () => {
     return (
-        <div>
-            <h1>Wax the Ripper</h1>
-            <p>Schedule your wax appointments easily and efficiently.</p>
+        <div className="home-container">
+            <h1 className="centered-title">Wax the Ripper</h1>
+            <p className="centered-text">Schedule your wax appointments easily and efficiently.</p>
         </div>
     );
 };
